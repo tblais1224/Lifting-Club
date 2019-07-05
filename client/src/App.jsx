033379ed221d023287ca1f0dd7a9be2a2b7a629f
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-05 11:35:13 -0400

    created react app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, Welcome to the future of working out.</h1>
+      <br />
+      <h1>LiftingClub.com</h1>
+      <div className="App-logo"></div>
+    </div>
+  );
+}
+
+export default App;
