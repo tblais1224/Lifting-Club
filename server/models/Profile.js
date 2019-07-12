1968b96ba10ebd1c7951f8b5d7f045e2682c5eaa
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-12 14:08:57 -0400

    fix profile schema call mongoose schema

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema
 
 const ProfileSchema = new mongoose.Schema({
   user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     required: true
   },
   social: {
