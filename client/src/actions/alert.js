c09dc98bd7c25524c0fa1fd887ce2a32d5b0cd18
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-17 17:13:00 -0400

    alerts working

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,14 @@
-import {SET_ALERT, REMOVE_ALERT} from "./types"
-import uuid from "uuid"
+import { SET_ALERT, REMOVE_ALERT } from "./types";
+import uuid from "uuid";
 
 //funk middleware allows dispatch
-export const setAlert = (msg, alertType) => dispatch => {
-    // get random universal id using uuid
-    const id = uuid.v4()
-    dispatch({
-        type: SET_ALERT,
-        payload: {msg, alertType, id}
-    })
-}
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+  // get random universal id using uuid
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  });
+  //remove alert after timeout, default is 5 seconds
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+};
