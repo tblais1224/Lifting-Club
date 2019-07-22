8915e24aeb8e91beea0007ade67f8821230d66dd
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-22 13:59:49 -0400

    working on dashboardcomponent

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -21,6 +21,11 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
         <Link to="/members">Members</Link>
       </li>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user"/>{' '}
+          <span className="hide-sm">Dashboard</span></Link>
+      </li>
+      <li>
         <a href="#!" onClick={logout}>
           <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
